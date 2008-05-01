f83d593ce557c0ceba3040e279c8506e72180a17
Horst Gutmann <zerok@zerokspot.com>
2008-05-01 09:59:09 +0200

    You can now set the number of pages that should be listed in your pagination using settings.PAGINATION_PAGE_LIMIT (default: 10)

--- templatetags/zsutils/pagination.py
+++ templatetags/zsutils/pagination.py
@@ -22,6 +22,12 @@ customize by overwriting the pagination.html template.
 In case the template variable has a different semantic for you, you can
 tell the pagination template tag to use a different context variable through
 settings.CONTEXT_PAGINATION_VARIABLE.
+
+By default, you will get 10 pages in your pagination (if your resultset has
+this many pages). You can configure this value again in your settings.py by
+setting PAGINATION_PAGE_LIMIT to whatever number of pages you like (note that
+a value below 3 hardly makes any sense, so you will get an error message if
+you still try that).
 """
 
 from django.template import Library
@@ -32,11 +38,22 @@ register = Library()
 @register.inclusion_tag('pagination.html', takes_context=True)
 def pagination(context):
     ctx_var = getattr(settings, 'CONTEXT_PAGINATION_VARIABLE', 'page')
+    page_limit = int(getattr(settings, 'PAGINATION_PAGE_LIMIT', 10))
+    
+    assert page_limit >= 2, "Set PAGINATION_PAGE_LIMIT to a value bigger than 2"
+    
     page = context.get(ctx_var, None)
     if page is None:
         return {'has_pagination':False}
-    previous_pages = range(1, page.number)[-5:]
-    next_pages = range(page.number+1, page.paginator.num_pages+1)[:5]
+    # Calculate the number of pages on each side of the current page
+    lower_half = (page_limit-(page_limit % 2 == 0 and 1 or 0))/2
+    previous_pages = range(1, page.number)[-lower_half:]
+    upper_half = (page_limit/2)
+    if len(previous_pages) < lower_half: upper_half += lower_half-len(previous_pages)
+    next_pages = range(page.number+1, page.paginator.num_pages+1)[:upper_half]
+    if len(next_pages) < upper_half:
+        lower_half += upper_half-len(next_pages)
+        previous_pages = range(1, page.number)[-lower_half:]
     last_page = page.paginator.num_pages
     return {
         'page': page,
