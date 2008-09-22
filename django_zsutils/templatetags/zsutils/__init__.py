a3e0aa45e650f5cbcc282af030b2789b5303e635
Horst Gutmann <zerok@zerokspot.com>
2008-09-22 10:22:24 +0200

    Simple templatetag for datetime properties

--- django_zsutils/templatetags/zsutils/__init__.py
+++ django_zsutils/templatetags/zsutils/__init__.py
@@ -2,3 +2,13 @@
 This module contains template tags for instance for a unified pagination
 look and feel.
 """
+from django.conf import settings
+from django.utils.dateformat import format
+from django import template
+
+register = template.Library()
+
+@register.filter('datetime')
+def datetime(value):
+    return format(value, settings.DATETIME_FORMAT)
+
